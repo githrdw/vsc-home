83930a6b88f8e3cc2f69fa2eddc64f1f27cfb843
Ruben <1455140+githrdw@users.noreply.github.com>
2021-03-12 18:58:30 +0100

    feat(extension): create default folders on init

--- packages/core/src/extension.ts
+++ packages/core/src/extension.ts
@@ -4,6 +4,7 @@ import * as vscode from 'vscode';
 import Api from './api';
 import MainWebview from './views/MainWebview';
 import SidebarWebview from './views/SidebarWebview';
+import InitialAppdata from './utils/InitAppdata';
 
 const EventBus = new Api();
 
@@ -26,14 +27,12 @@ const openSidebarView = ({ extensionPath }: vscode.ExtensionContext) => {
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vsch" is now active!');
-
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
+
+	InitialAppdata();
+
 	const mainViewCommand = vscode.commands.registerCommand('vsch.openMainView', async () => {
 		// The code you place here will be executed every time your command is executed
 		openMainView(context);
---
+++ packages/core/src/utils/InitAppdata.ts
@@ -0,0 +1,26 @@
+import { Uri, workspace, window } from "vscode";
+import { join } from 'path';
+import vars from '../vars';
+
+export default async function () {
+  const root = Uri.file(vars.USR_APP_DIR);
+  try {
+    await workspace.fs.createDirectory(root);
+  } catch (e) {
+    window.showErrorMessage(`Not able to create configuration directory in AppData: ${e.toString()}`);
+  }
+
+  const widgets = Uri.file(join(vars.USR_APP_DIR, vars.WIDGETS_ROOT));
+  try {
+    await workspace.fs.createDirectory(widgets);
+  } catch (e) {
+    console.warn(`Not able to create widgets directory in configuration directory: ${e.toString()}`);
+  }
+
+  const layout = Uri.file(join(vars.USR_APP_DIR, vars.LAYOUTS_ROOT, 'default.json'));
+  try {
+    await workspace.fs.writeFile(layout, new Uint8Array());
+  } catch (e) {
+    console.warn(`Not able to create widgets directory in configuration directory: ${e.toString()}`);
+  }
+}
